8a293fea5c251f65f5fcf93d2a606e496c10db55
Martin Fahlström <makrill88@gmail.com>
2024-04-24 15:45:01 +0200

    cart styling

--- src/components/ShoppingCartItem.jsx
+++ src/components/ShoppingCartItem.jsx
@@ -1,19 +1,23 @@
 import React from "react";
+import "../css/shopping-cart-items.css";
 
 export default function ShoppingCartItem() {
   return (
-    <div className="items-container">
-      <div className="shopping-cart-item">
-        <img src="" alt="produkt" />
-        <h2>Titel</h2>
-        <div>
-          <button>-</button>
-          <p>antal</p>
-          <button>+</button>
-        </div>
-        <p>pris</p>
-        <p>totalt pris</p>
+    <div className="cart-item">
+      <div className="item-image">
+        <img
+          src="https://static.vecteezy.com/system/resources/previews/009/974/271/original/3d-rendering-beach-ball-isolated-png.png"
+          alt="produkt"
+        />
+        <p>Badboll XL</p>
       </div>
+      <p className="item-price">pris</p>
+      <div className="item-quantity">
+        <button>-</button>
+        <p>1</p>
+        <button>+</button>
+      </div>
+      <p className="item-price">199kr</p>
     </div>
   );
 }
--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -6,28 +6,37 @@ import "../css/shopping-cart.css";
 
 export default function ShoppingCart() {
   return (
-    <section className="shopping-cart-section">
+    <>
       <Header />
-      <NavLink to="/">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="black"
-          className="w-6 h-6"
-          width="20px"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
-          />
-        </svg>
-        Back
-      </NavLink>
-      <h1 className="shopping-cart-title">Dina Varukorg</h1>
-      <ShoppingCartItem />
-    </section>
+      <section className="cart">
+        <NavLink to="/">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="black"
+            className="w-6 h-6"
+            width="20px"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+            />
+          </svg>
+          Back
+        </NavLink>
+        <h1 className="cart-title">Dina Varukorg</h1>
+        <div className="cart-header">
+          <p className="item-name">Produkt</p>
+          <p className="item-price">Pris</p>
+          <p className="item-quantity">Antal</p>
+          <p className="item-price">á pris</p>
+          <p className="item-total-price">Totalt</p>
+        </div>
+        <ShoppingCartItem />
+      </section>
+    </>
   );
 }
