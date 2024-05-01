45b191986228fbfa9741e478af5b1560bbfbffbb
Martin Fahlström <makrill88@gmail.com>
2024-05-01 12:27:35 +0200

    thank you page styling

--- src/pages/ThankYoypage.jsx
+++ src/pages/ThankYoypage.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "../css/thanks.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+
 export default function ThankYoypage() {
   return (
-    <div className="thanks-wrapper">
-      <h1>Tack för din order!</h1>
-      <NavLink to="/" className="thanks-link">
-        Fortsätt shoppa
-      </NavLink>
-    </div>
+    <>
+      <div className="thanks-link-wrapper">
+        <NavLink to="/" className="thanks-link">
+          X
+        </NavLink>
+      </div>
+      <div className="thanks-wrapper">
+        <FontAwesomeIcon className="checkmark-icon" icon={faCheck} />
+        <h1>Tack för din Beställning!</h1>
+        <p>Dina varor skickas inom 3-5 dagar.</p>
+      </div>
+    </>
   );
 }
